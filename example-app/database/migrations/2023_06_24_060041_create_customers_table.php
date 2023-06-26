90459db93cc0abff77d3368ca16e25f3beb7e4d7
gvk2800 <gauraavkumarcs2800@gmail.com>
2023-06-26 10:24:49 +0530

    end_pr

--- example-app/database/migrations/2023_06_24_060041_create_customers_table.php
+++ example-app/database/migrations/2023_06_24_060041_create_customers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('role_id')->nullable();
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('mobile');
